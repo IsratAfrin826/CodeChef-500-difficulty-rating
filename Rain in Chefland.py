5193ded8999cdc78d9711de74664c675292e704b
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-09-18 20:29:24 +0600

    Add files via upload

---
+++ Rain in Chefland.py
@@ -0,0 +1,17 @@
+T = int(input())
+
+for _ in range(T):
+
+    X = int(input())
+
+    if X < 3:
+
+        print("LIGHT")
+
+    elif X < 7:
+
+        print("MODERATE")
+
+    else:
+
+        print("HEAVY")
