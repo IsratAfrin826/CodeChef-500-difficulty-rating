4ee963ea739657261c2a027021b30340e561c108
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-09-29 12:30:33 +0600

    Add files via upload

---
+++ Overspeeding Fine.py
@@ -0,0 +1,17 @@
+T = int(input())
+
+for _ in range(T):
+
+    X = int(input())
+
+    if X <= 70:
+
+        print(0)
+
+    elif X <= 100:
+
+        print(500)
+
+    else:
+
+        print(2000)
